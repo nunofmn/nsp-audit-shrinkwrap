be9e8e1ba2da10715d8a6c0307780de59ff9acbb
Tom Steele <thomasjsteele@gmail.com>
2014-09-20 19:02:46 -0700

    Update test-shrinkwrap.js

--- tests/test-shrinkwrap.js
+++ tests/test-shrinkwrap.js
@@ -11,16 +11,16 @@ var pNotVul = fs.readFileSync(pNotVulPath);
 
 ///
 
-test('shrinkwrap with no vulnerabilties test', function(t) {
-    nsp_shrinkwrap.audit(pNotVul, function(err, results) {
+test('shrinkwrap with no vulnerabilties test', function (t) {
+    nsp_shrinkwrap.audit(pNotVul, function (err, results) {
         t.error(err, 'Should not return a error');
         t.same(results, [], 'Should not contain any vulnerabilities');
         t.end();
     });
 });
 
-test('shrinkwrap with vulnerabilties test', function(t) {
-    nsp_shrinkwrap.audit(pVul, function(err, results) {
+test('shrinkwrap with vulnerabilties test', function (t) {
+    nsp_shrinkwrap.audit(pVul, function (err, results) {
         t.error(err, 'Should not return a error');
         t.equal(results.length, 5, 'Should return 5 vulnerabilties');
         t.end();
@@ -30,16 +30,16 @@ test('shrinkwrap with vulnerabilties test', function(t) {
 
 ///
 
-test('shrinkwrap path with no vulnerabilties test test', function(t) {
-    nsp_shrinkwrap.auditByPath(pNotVulPath, function(err, results) {
+test('shrinkwrap path with no vulnerabilties test test', function (t) {
+    nsp_shrinkwrap.auditByPath(pNotVulPath, function (err, results) {
         t.error(err, 'Should not return a error');
         t.same(results, [], 'Should not contain any vulnerabilities');
         t.end();
     });
 });
 
-test('shrinkwrap path with vulnerabilties test test', function(t) {
-    nsp_shrinkwrap.auditByPath(pVulPath, function(err, results) {
+test('shrinkwrap path with vulnerabilties test test', function (t) {
+    nsp_shrinkwrap.auditByPath(pVulPath, function (err, results) {
         t.error(err, 'Should not return a error');
         t.equal(results.length, 5, 'Should return 5 vulnerabilties');
         t.end();
@@ -49,21 +49,21 @@ test('shrinkwrap path with vulnerabilties test test', function(t) {
 
 ///
 
-test('shrinkwrap stream test', function(t) {
+test('shrinkwrap stream test', function (t) {
     var auditStream = nsp_shrinkwrap.auditStream();
     var results = [];
     auditStream.shrinkwrap.write(pNotVul);
     auditStream.shrinkwrap.write(pVul);
 
-    setTimeout(function() {
+    setTimeout(function () {
         auditStream.shrinkwrap.end();
     }, 5000); // value depends on how responsive is the API
 
-    auditStream.results.on('_data', function(data) {
+    auditStream.results.on('_data', function (data) {
         results.push(data);
     });
 
-    auditStream.results.on('_end', function() {
+    auditStream.results.on('_end', function () {
         t.equal(results.length, 2, 'Should have two results, one for each shrinkwrap');
         t.equal(results[0].length + results[1].length, 5, 'Sum of shrinkwrap vulns should be 5');
         t.end();
